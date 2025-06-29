26040a9938cd582fc8ab369a2a2af4c6d5faf7d8
Yuchan <songyc0324@gmail.com>
2025-06-29 15:43:39 +0900

    Update: Game Container

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -19,13 +19,13 @@ const Game = () => {
       <Date>{months.find(key => key.includes("24일"))}</Date>
       <Time>{testData.time}</Time>
       <TeamImg src={Tigers}/>
-      <AwayTeam>{testData.away}</AwayTeam>
-      <AwayScore>{testData.awayScore}</AwayScore>
+      <Team>{testData.away}</Team>
+      <Score>{testData.awayScore}</Score>
       <Stadium>{testData.stadium}</Stadium>
       
       <TeamImg src={Heroes}/>
-      <HomeTeam>{testData.home}</HomeTeam>
-      <HomeScore>{testData.homeScore}</HomeScore>
+      <Team>{testData.home}</Team>
+      <Score>{testData.homeScore}</Score>
     </Layout>
   );
 };
@@ -36,39 +36,35 @@ const Layout = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-  // flex-direction: column;
+  align-items: center;
 `
 
 const Date = styled.div`
   width: 100%;
-  height: 40px;
-  border: 1px solid black;
 
 `
 const Time = styled.div`
-  width: ;
-  border: 1px solid black;
-
+  width: 10%;
+  
+  text-align: center;
 `
 const Stadium = styled.div`
+  width: 10%;
+  
+  text-align: center;
+`
+const Team = styled.div`
   width: 30%;
-  border: 1px solid black;
+  
 `
-const AwayTeam = styled.div`
-  border: 1px solid black;
-`
-const AwayScore = styled.div`
-  border: 1px solid black;
-`
-const HomeTeam = styled.div`
-  border: 1px solid black;
-`
-const HomeScore = styled.div`
-  border: 1px solid black;
+const Score = styled.div`
+  width: 48%;
+  
 `
 
 const TeamImg = styled.img`
-  width: 40px;
-  border: 1px solid black;
+  width: 10%;
+  
+
 `
 export default Game;
