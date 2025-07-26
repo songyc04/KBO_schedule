af41cd7af65a054b93f38523a0baa4d482e9cbbd
Yuchan <songyc0324@gmail.com>
2025-07-26 23:22:52 +0900

    Update: Game Information

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,22 +2,46 @@ import styled from "styled-components";
 
 import GameData from "#data/gamedata.json";
 
+import Bears from "#images/Bears.png";
+import Dinos from "#images/Dinos.png";
+import Eagles from "#images/Eagles.png";
+import Giants from "#images/Giants.png";
 import Heroes from "#images/Heroes.png";
+import Landers from "#images/Landers.png";
+import Lions from "#images/Lions.png";
 import Tigers from "#images/Tigers.png";
+import Twins from "#images/Twins.png";
+import Wiz from "#images/Wiz.png";
 
+
+const logo = {
+  두산: Bears,
+  NC: Dinos,
+  한화: Eagles,
+  롯데: Giants,
+  키움: Heroes,
+  SSG: Landers,
+  삼성: Lions,
+  KIA: Tigers,
+  LG: Twins,
+  KT: Wiz
+};
 
 const Game = ({ date, dateData }) => {
+  const awayLogo = logo[dateData.away];
+  const homeLogo = logo[dateData.home]
+
 
   return (
     <Layout>
       <Date>{date}</Date>
       <Time>{dateData.time}</Time>
-      <TeamImg src={Tigers}/>
+      <TeamImg src={awayLogo}/>
       <Team>{dateData.away}</Team>
       <Score>{dateData.awayScore}</Score>
       <Stadium>{dateData.stadium}</Stadium>
       
-      <TeamImg src={Heroes}/>
+      <TeamImg src={homeLogo}/>
       <Team>{dateData.home}</Team>
       <Score>{dateData.homeScore}</Score>
     </Layout>
